95fa293584de8c77ff3d955f6f7b8614488e17c3
shabari shenoy <sss_shenoy06@hotmail.com>
2018-09-23 12:50:36 -0400

    - generate code for user - twilio send out code - firebase store in db not auth - store code in db under users collection/phone - mark the code as 'in use' so cant be used by other

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin');
+const twilio = require('./twilio');
 
 module.exports = function(req, res){
     if(!req.body.phone){
@@ -9,7 +10,28 @@ module.exports = function(req, res){
 
     admin.auth().getUser(phone)
         .then(userRecord => {
-            
+            // get a number anywhere from 1000 to 8999
+            const code = Math.floor((Math.random() * 8999 + 1000));
+
+            // twilio create is asynchronous but DOES NOT return a promise so cannot do then
+            // instead its more traditional so we have to use a callback ... the () => after object
+            twilio.messages.create({
+                body: 'Your Code for flutr is: ' + code,
+                to: phone,
+                from: '6479300299'
+            }, (err) => {
+                // same production rules as below apply here (right now were debugging twilio)
+                if(err){ return res.status(422).send(err); }
+
+                // real big gotcha with FIREBASE
+                // there are protected properties like created_at in the authentication tab of firebase
+                // so instead we can use the FIREBASE Db since we can make up property values
+                // so there is no phone in auth i think
+                admin.database().ref('users/' + phone)
+                    .update({ code: code, codeValid: true }, () => {
+                        res.send({ success: true });
+                    });
+            });
         })
         .catch((err) => {
             // if user is not found
